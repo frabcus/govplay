547e738537a7e05b1828cd8bed1cd006a51acc89
Francis Irving <francis@flourish.org>
2014-10-01 13:42:00 +0100

    Changes paths

---
+++ via-template/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.4
+
+import pystache
+
+template = open("govuk_template_mustache/views/layouts/govuk_template.html").read()
+
+params = {
+    "pageTitle": "Civil Service Survey",
+    "assetPath": "govuk_template_mustache/assets/",
+    "content": """
+        <div id="wrapper">
+        <main id="content">
+       <p> This is the content :)</p>
+       </main>
+       </div>
+    """,
+
+    "headerClass": "with-proposition",
+    "propositionHeader": """
+<div class="header-proposition">
+  <div class="content">
+    <nav id="proposition-menu" role="navigation">
+      <a href="/" id="proposition-name">Civil Service Survey</a>
+      <span class="alpha-tag">Alpha</span>
+      <a href="#proposition-links" class="js-header-toggle menu">Menu</a>
+      <ul id="proposition-links">
+        <li><a href="#">Login</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+"""
+}
+
+html = pystache.render(template, params)
+outfile = open("out.html", "w")
+outfile.write(html)
+
